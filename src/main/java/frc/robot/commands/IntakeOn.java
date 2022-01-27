42bae8e4ef9aff6d5683c02575e08ebb715ca9de
Britton <you@example.com>
2022-01-27 17:59:14 -0600

    linting

--- src/main/java/frc/robot/commands/IntakeOn.java
+++ src/main/java/frc/robot/commands/IntakeOn.java
@@ -3,7 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
-public class IntakeOn extends CommandBase{
+/**
+ * Initalizes the intake object for method use
+ */
+public class IntakeOn extends CommandBase {
     private final Intake intake;
 
     public IntakeOn(Intake subsytem) {
