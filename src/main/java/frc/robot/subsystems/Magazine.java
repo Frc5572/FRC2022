257c611bb19903e616893f22338656591332aa13
Shyam <shyamdayal@sbcglobal.net>
2022-01-27 18:15:17 -0600

    sensor

--- src/main/java/frc/robot/subsystems/Magazine.java
+++ src/main/java/frc/robot/subsystems/Magazine.java
@@ -1,22 +1,42 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Magazine extends SubsystemBase {
     private WPI_TalonSRX magazineMotor = new WPI_TalonSRX(Constants.magazinecanID);
-    DigitalInput climb = new DigitalInput(0);
+    private boolean intaking;
+
+
 
     public void up() {
+        intaking = true;
         magazineMotor.set(.5);
 
     }
 
-    public void stop() {
-        magazineMotor.set(0);
+    /*
+     * public void stop() { magazineMotor.set(0); }
+     */
 
+
+
+    @Override
+    protected boolean isFinished() {
+        if (intaking) {
+            return !Neptune.trident.infrared.get();
+        } else {
+            return isTimedOut();
+            // ||Neptune.trident.infrared.get();
+        }
     }
+
+    @Override
+    protected void end() {
+        Neptune.trident.stopIntake();
+    }
+
+
 
 }
