6013681c6b637858f08495e51829630731875034
ehernandez6800 <ehernandez6800@stu.neisd.net>
2022-01-31 17:20:51 -0600

    Turret code done

--- src/main/java/frc/robot/commands/RightTurretMove.java
+++ src/main/java/frc/robot/commands/RightTurretMove.java
@@ -3,6 +3,9 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Turret;
 
+/**
+ * Right turret move command.
+ */
 public class RightTurretMove extends CommandBase {
 
     private Turret turret;
