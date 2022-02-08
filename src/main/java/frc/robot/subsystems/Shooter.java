96a2ced80c90da543d750e0f6bbb21f054037761
Aidan Underwood <aidanunderwood1@icloud.com>
2022-02-08 17:25:45 -0600

    final lint

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -54,11 +54,11 @@ public class Shooter extends PIDSubsystem {
         if (m_enabled) {
             useOutput(m_controller.calculate(getMeasurement(), getSetpoint()), getSetpoint());
             double selSenVel = shooter.getSelectedSensorVelocity(0);
-            double RotPerSec = (double) selSenVel / Constants.ShooterPID.kUnitsPerRevolution
+            double rotPerSec = (double) selSenVel / Constants.ShooterPID.kUnitsPerRevolution
                 * 10; /* scale per100ms to perSecond */
-            double RotPerMin = RotPerSec * 60.0;
+            double rotPerMin = rotPerSec * 60.0;
 
-            System.out.println("RPM (Speed): " + RotPerMin);
+            System.out.println("RPM (Speed): " + rotPerMin);
             System.out.println("Voltage: " + shooter.getMotorOutputVoltage());
         }
     }
