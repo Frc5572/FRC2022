8ca674bce434a26eb28e8bd12fbd3d9941766325
owenflatman <owenjflatman@gmail.com>
2022-02-07 19:15:43 -0600

    Progress

--- src/main/java/frc/robot/autos/P3_4B.java
+++ src/main/java/frc/robot/autos/P3_4B.java
@@ -14,7 +14,7 @@ import frc.robot.subsystems.Swerve;
 /**
  * Autonomous that aligns limelight then excecutes a trajectory.
  */
-public class TestAuto extends SequentialCommandGroup {
+public class P3_4B extends SequentialCommandGroup {
     Swerve swerve;
 
     /**
@@ -22,7 +22,7 @@ public class TestAuto extends SequentialCommandGroup {
      *
      * @param swerve swerve subsystem
      */
-    public TestAuto(Swerve swerve) {
+    public P3_4B(Swerve swerve) {
         this.swerve = swerve;
         System.out.println("Test Auto !!");
         // TrajectoryConfig config =
@@ -36,16 +36,33 @@ public class TestAuto extends SequentialCommandGroup {
         // new Pose2d(1, 1, new Rotation2d(0))),
         // config);
 
-        PathPlannerTrajectory examplePath = PathPlanner.loadPath("Rusinski's", 1, 1);
+        PathPlannerTrajectory Part1 = PathPlanner.loadPath("Part1", 1, 1);
+        PathPlannerTrajectory Part2 = PathPlanner.loadPath("Part2", 1, 1);
+        PathPlannerTrajectory Part3 = PathPlanner.loadPath("Part3", 1, 1);
 
         var thetaController = new ProfiledPIDController(Constants.AutoConstants.kPThetaController,
             0, 0, Constants.AutoConstants.kThetaControllerConstraints);
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
-        PPSwerveControllerCommand firstHalfTraject = new PPSwerveControllerCommand(examplePath,
-            swerve::getPose, Constants.Swerve.swerveKinematics,
-            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
-            new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
-            swerve::setModuleStates, swerve);
+
+        PPSwerveControllerCommand grabSecondBall =
+            new PPSwerveControllerCommand(Part1, swerve::getPose, Constants.Swerve.swerveKinematics,
+                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
+                swerve::setModuleStates, swerve);
+
+        PPSwerveControllerCommand grabThirdBall =
+            new PPSwerveControllerCommand(Part2, swerve::getPose, Constants.Swerve.swerveKinematics,
+                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
+                swerve::setModuleStates, swerve);
+
+        PPSwerveControllerCommand grabFourthBall =
+            new PPSwerveControllerCommand(Part2, swerve::getPose, Constants.Swerve.swerveKinematics,
+                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
+                swerve::setModuleStates, swerve);
+
+
         // PPSwerveControllerCommand firstHalfTraject = new PPSwerveControllerCommand(examplePath,
         // swerve::getPose, Constants.Swerve.swerveKinematics,
         // new PIDController(Constants.AutoConstants.kPXController, 0, 0),
@@ -54,7 +71,7 @@ public class TestAuto extends SequentialCommandGroup {
         ZeroMotorsWaitCommand firstWait = new ZeroMotorsWaitCommand(swerve, 3);
         ZeroMotorsWaitCommand secondWait = new ZeroMotorsWaitCommand(swerve, .5);
 
-        addCommands(new InstantCommand(() -> swerve.resetOdometry(examplePath.getInitialPose())),
-            firstHalfTraject);
+        addCommands(new InstantCommand(() -> swerve.resetOdometry(Part1.getInitialPose())),
+            grabSecondBall);
     }
 }
