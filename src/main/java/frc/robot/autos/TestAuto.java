4dbc0bfaec57c4edd7c79bb150943c2ae3863458
Britton <you@example.com>
2022-02-10 17:05:17 -0600

    progress

--- src/main/java/frc/robot/autos/TestAuto.java
+++ src/main/java/frc/robot/autos/TestAuto.java
@@ -36,12 +36,14 @@ public class TestAuto extends SequentialCommandGroup {
         // new Pose2d(1, 1, new Rotation2d(0))),
         // config);
 
-        PathPlannerTrajectory examplePath = PathPlanner.loadPath("Rusinski's", 1, 1);
+        PathPlannerTrajectory P2B2 = PathPlanner.loadPath("P2B2", 1, 1);
 
         var thetaController = new ProfiledPIDController(Constants.AutoConstants.kPThetaController,
             0, 0, Constants.AutoConstants.kThetaControllerConstraints);
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
-        PPSwerveControllerCommand firstHalfTraject = new PPSwerveControllerCommand(examplePath,
+
+        PPSwerveControllerCommand Auto = new PPSwerveControllerCommand(
+            P2B2,
             swerve::getPose, Constants.Swerve.swerveKinematics,
             new PIDController(Constants.AutoConstants.kPXController, 0, 0),
             new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
@@ -54,7 +56,7 @@ public class TestAuto extends SequentialCommandGroup {
         ZeroMotorsWaitCommand firstWait = new ZeroMotorsWaitCommand(swerve, 3);
         ZeroMotorsWaitCommand secondWait = new ZeroMotorsWaitCommand(swerve, .5);
 
-        addCommands(new InstantCommand(() -> swerve.resetOdometry(examplePath.getInitialPose())),
-            firstHalfTraject);
+        addCommands(new InstantCommand(() -> swerve.resetOdometry(P2B2.getInitialPose())),
+            Auto);
     }
 }
