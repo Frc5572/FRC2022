3c9a0247f06872af9a37687b5cd98141ee4b4516
aidan-underwood <61338453+aidan-underwood@users.noreply.github.com>
2022-02-09 22:16:43 -0600

    zero idea if this pid command works but its something

--- src/main/java/frc/robot/commands/MagControl.java
+++ src/main/java/frc/robot/commands/MagControl.java
@@ -1,31 +1,35 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants.MagazinePID;
 import frc.robot.subsystems.Magazine;
 
 /**
  * Creates commands for the magazine to run.
  */
-public class MagControl extends ForceMag {
+public class MagControl extends PIDCommand {
     private Magazine magazine;
-    private ForceMag forceMag;
 
     /**
      * Initalizes the command for magazine and adds requirements to systems.
      */
-    public MagControl(Magazine magazine, ForceMag forceMag) {
-        super(magazine);
-        this.forceMag = forceMag;
-        this.magazine = magazine;
+    public MagControl(Magazine magazine) {
+        super(new PIDController(MagazinePID.kP, MagazinePID.kI, MagazinePID.kD),
+            () -> (magazine.getMeasurement()), () -> 10, output -> {
+                magazine.periodic();
+            });
         addRequirements(magazine);
     }
 
-    @Override
-    public void execute() {
-        magazine.startMagazine();
+    public void initialize() {
+        super.initialize();
     }
 
     @Override
     public boolean isFinished() {
+        final Boolean interrupted = magazine.magSense.get();
+        super.end(interrupted);
         return magazine.magSense.get();
     }
 }
