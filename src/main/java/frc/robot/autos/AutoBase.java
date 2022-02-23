6dd57cfa3cdac24fdb7c5917efe5470187a4c3ed
Alex Resnick <adr8292@gmail.com>
2022-02-23 17:31:00 -0600

    Create Auto Parent Class

---
+++ src/main/java/frc/robot/autos/AutoBase.java
@@ -0,0 +1,23 @@
+package frc.robot.autos;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+public class AutoBase extends SequentialCommandGroup {
+    Swerve swerve;
+    ProfiledPIDController thetaController =
+        new ProfiledPIDController(Constants.AutoConstants.kPThetaController, 0, 0,
+            Constants.AutoConstants.kThetaControllerConstraints);
+
+    /**
+     * Autonomous that aligns limelight then excecutes a trajectory.
+     *
+     * @param swerve swerve subsystem
+     */
+    public AutoBase(Swerve swerve) {
+        this.swerve = swerve;
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    }
+}
