5887bd199836e92ccda97f8e14cdf12e755966fb
Alex Resnick <adr8292@gmail.com>
2022-02-24 09:11:10 -0600

    add java docs

--- src/main/java/frc/robot/modules/AutoBase.java
+++ src/main/java/frc/robot/modules/AutoBase.java
@@ -29,6 +29,12 @@ public class AutoBase extends SequentialCommandGroup {
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
     }
 
+    /**
+     * Creates a SwerveControllerCommand from a Trajectory
+     *
+     * @param trajectory Trajectory to run
+     * @return A SwerveControllerCommand for the robot to move
+     */
     public SwerveControllerCommand baseSwerveCommand(Trajectory trajectory) {
         SwerveControllerCommand command = new SwerveControllerCommand(trajectory, swerve::getPose,
             Constants.Swerve.swerveKinematics,
@@ -38,7 +44,12 @@ public class AutoBase extends SequentialCommandGroup {
         return command;
     }
 
-
+    /**
+     * Creates a SwerveController Command using a Path Planner Trajectory
+     *
+     * @param trajectory a Path Planner Trajectory
+     * @return A SwerveControllerCommand for the robot to move
+     */
     public PPSwerveControllerCommand baseSwerveCommand(PathPlannerTrajectory trajectory) {
         PPSwerveControllerCommand command = new PPSwerveControllerCommand(trajectory,
             swerve::getPose, Constants.Swerve.swerveKinematics,
