de404879f6ed710f64520c69831000b267211208
Alex Resnick <adr8292@gmail.com>
2022-02-24 16:31:36 -0600

    Create Auto Parent Class (#79)
    Co-authored-by: agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>

--- src/main/java/frc/robot/autos/TestAuto.java
+++ src/main/java/frc/robot/autos/TestAuto.java
@@ -3,19 +3,14 @@ package frc.robot.autos;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants;
-import frc.robot.commands.ZeroMotorsWaitCommand;
+import frc.robot.modules.AutoBase;
 import frc.robot.subsystems.Swerve;
 
 /**
  * Autonomous that aligns limelight then excecutes a trajectory.
  */
-public class TestAuto extends SequentialCommandGroup {
-    Swerve swerve;
+public class TestAuto extends AutoBase {
 
     /**
      * Autonomous that aligns limelight then excecutes a trajectory.
@@ -23,38 +18,12 @@ public class TestAuto extends SequentialCommandGroup {
      * @param swerve swerve subsystem
      */
     public TestAuto(Swerve swerve) {
-        this.swerve = swerve;
-        System.out.println("Test Auto !!");
-        // TrajectoryConfig config =
-        // new TrajectoryConfig(Constants.AutoConstants.kMaxSpeedMetersPerSecond,
-        // Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-        // .setKinematics(Constants.Swerve.swerveKinematics);
-
-        // Trajectory firstHalfTrajectory = TrajectoryGenerator.generateTrajectory(
-
-        // List.of(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(1, 0, new Rotation2d(0)),
-        // new Pose2d(1, 1, new Rotation2d(0))),
-        // config);
+        super(swerve);
 
         PathPlannerTrajectory examplePath = PathPlanner.loadPath("Rusinski's", 1, 1);
-
-        var thetaController = new ProfiledPIDController(Constants.AutoConstants.kPThetaController,
-            0, 0, Constants.AutoConstants.kThetaControllerConstraints);
-        thetaController.enableContinuousInput(-Math.PI, Math.PI);
-        PPSwerveControllerCommand firstHalfTraject = new PPSwerveControllerCommand(examplePath,
-            swerve::getPose, Constants.Swerve.swerveKinematics,
-            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
-            new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
-            swerve::setModuleStates, swerve);
-        // PPSwerveControllerCommand firstHalfTraject = new PPSwerveControllerCommand(examplePath,
-        // swerve::getPose, Constants.Swerve.swerveKinematics,
-        // new PIDController(Constants.AutoConstants.kPXController, 0, 0),
-        // new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
-        // swerve::setModuleStates, swerve);
-        ZeroMotorsWaitCommand firstWait = new ZeroMotorsWaitCommand(swerve, 3);
-        ZeroMotorsWaitCommand secondWait = new ZeroMotorsWaitCommand(swerve, .5);
+        PPSwerveControllerCommand testCommand = baseSwerveCommand(examplePath);
 
         addCommands(new InstantCommand(() -> swerve.resetOdometry(examplePath.getInitialPose())),
-            firstHalfTraject);
+            testCommand);
     }
 }
---
+++ src/main/java/frc/robot/modules/AutoBase.java
@@ -0,0 +1,61 @@
+package frc.robot.modules;
+
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+/**
+ * Parent class for all autonomous commands
+ */
+public class AutoBase extends SequentialCommandGroup {
+    public Swerve swerve;
+    public ProfiledPIDController thetaController =
+        new ProfiledPIDController(Constants.AutoConstants.kPThetaController, 0, 0,
+            Constants.AutoConstants.kThetaControllerConstraints);
+
+    /**
+     * Autonomous that aligns limelight then excecutes a trajectory.
+     *
+     * @param swerve swerve subsystem
+     */
+    public AutoBase(Swerve swerve) {
+        this.swerve = swerve;
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    }
+
+    /**
+     * Creates a SwerveControllerCommand from a Trajectory
+     *
+     * @param trajectory Trajectory to run
+     * @return A SwerveControllerCommand for the robot to move
+     */
+    public SwerveControllerCommand baseSwerveCommand(Trajectory trajectory) {
+        SwerveControllerCommand command = new SwerveControllerCommand(trajectory, swerve::getPose,
+            Constants.Swerve.swerveKinematics,
+            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+            new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
+            swerve::setModuleStates, swerve);
+        return command;
+    }
+
+    /**
+     * Creates a SwerveController Command using a Path Planner Trajectory
+     *
+     * @param trajectory a Path Planner Trajectory
+     * @return A SwerveControllerCommand for the robot to move
+     */
+    public PPSwerveControllerCommand baseSwerveCommand(PathPlannerTrajectory trajectory) {
+        PPSwerveControllerCommand command = new PPSwerveControllerCommand(trajectory,
+            swerve::getPose, Constants.Swerve.swerveKinematics,
+            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+            new PIDController(Constants.AutoConstants.kPYController, 0, 0), thetaController,
+            swerve::setModuleStates, swerve);
+        return command;
+    }
+}
