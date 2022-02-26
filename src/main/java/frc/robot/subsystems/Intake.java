4cf64ba7000b58a5bca97b5f65dbee76c87090f1
Aidan Underwood <61338453+aidan-underwood@users.noreply.github.com>
2022-02-26 14:57:41 -0600

    Merge Auto P1_2B/P3_2B into Main (#84)
    Co-authored-by: Shyam <shyamdayal@sbcglobal.net>
    Co-authored-by: Alex Grinman <90881143+gvaldez7206@users.noreply.github.com>
    Co-authored-by: Alex Resnick <adr8292@gmail.com>

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -36,10 +36,6 @@ public class Intake extends SubsystemBase {
         this.intakeSol.set(true);
     }
 
-    public void in() {
-        this.intakeMotor.set(.8);
-    }
-
     public void stop() {
         this.intakeMotor.set(0);
     }
