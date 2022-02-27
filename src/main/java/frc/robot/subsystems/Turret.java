92e1dda98f2a4f72c13d7a28cce53b34fdb6a9ab
Alex Resnick <adr8292@gmail.com>
2022-02-26 22:51:55 -0600

    update turret CAN status

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -11,6 +12,18 @@ public class Turret extends SubsystemBase {
     private final WPI_TalonSRX turretMotor = new WPI_TalonSRX(Constants.Motors.turretMotorID);
     public static final double spinLeft = -0.2;
     public boolean alignEnabled = true;
+
+    /**
+     * Constructs the Turret Subsystem
+     */
+    public Turret() {
+        turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 5000);
+        turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_Brushless_Current, 1000);
+        turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 5000);
+        turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 5000);
+        turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 5000);
+        turretMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1000);
+    }
 
     public void turretLeft() {
         turretMotor.set(spinLeft);
