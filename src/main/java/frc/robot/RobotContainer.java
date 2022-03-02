b0b3b74a689bb4c08adc4b1ca07f0db3f0fb85c3
Aidan Underwood <aidanunderwood1@icloud.com>
2022-03-02 11:29:10 -0600

    fix build

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,7 +63,7 @@ public class RobotContainer {
     private final Intake intake;
     private final Turret turret = new Turret();
     private Vision vision = new Vision();
-    private final Shooter shooter = new Shooter(vision);
+    private final Shooter shooter = new Shooter();
     // private final Hood hood = new Hood(vision);
     private final InsideClimber insideClimber;
     private final OutsideClimber outsideClimber;
