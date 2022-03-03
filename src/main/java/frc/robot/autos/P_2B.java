85e20f6ab4cc6a6445b18f5066c4c5511a327deb
agrinmanriv0537 <agrinmanriv@gmail.com>
2022-03-02 23:50:55 -0600

    without autos - need to make sure yaw values

--- src/main/java/frc/robot/autos/P_2B.java
+++ src/main/java/frc/robot/autos/P_2B.java
@@ -1,74 +1,74 @@
-package frc.robot.autos;
+// package frc.robot.autos;
 
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-import org.photonvision.PhotonCamera;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.commands.AlignTurret;
-import frc.robot.commands.ZeroMotorsWaitCommand;
-import frc.robot.modules.AutoBase;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Magazine;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Swerve;
-import frc.robot.subsystems.Turret;
+// import com.pathplanner.lib.PathPlanner;
+// import com.pathplanner.lib.PathPlannerTrajectory;
+// import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+// import org.photonvision.PhotonCamera;
+// import edu.wpi.first.wpilibj2.command.InstantCommand;
+// import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+// import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+// import edu.wpi.first.wpilibj2.command.WaitCommand;
+// import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+// import frc.robot.commands.AlignTurret;
+// import frc.robot.commands.ZeroMotorsWaitCommand;
+// import frc.robot.modules.AutoBase;
+// import frc.robot.subsystems.Intake;
+// import frc.robot.subsystems.Magazine;
+// import frc.robot.subsystems.Shooter;
+// import frc.robot.subsystems.Swerve;
+// import frc.robot.subsystems.Turret;
 
-/**
- * Autonomous that aligns limelight then excecutes a trajectory.
- */
-public class P_2B extends AutoBase {
+// /**
+// * Autonomous that aligns limelight then excecutes a trajectory.
+// */
+// public class P_2B extends AutoBase {
 
-    Intake intake;
-    Shooter shooter;
-    Magazine magazine;
+// Intake intake;
+// Shooter shooter;
+// Magazine magazine;
 
-    /**
-     * Autonomous that aligns limelight then excecutes a trajectory.
-     *
-     * @param swerve swerve subsystem
-     */
-    public P_2B(Swerve swerve, Shooter shooter, Magazine magazine, Intake intake, Turret turret,
-        PhotonCamera camera) {
-        super(swerve);
-        this.shooter = shooter;
-        this.magazine = magazine;
-        this.intake = intake;
-        addRequirements(shooter, magazine, intake, turret);
+// /**
+// * Autonomous that aligns limelight then excecutes a trajectory.
+// *
+// * @param swerve swerve subsystem
+// */
+// public P_2B(Swerve swerve, Shooter shooter, Magazine magazine, Intake intake, Turret turret,
+// PhotonCamera camera) {
+// super(swerve);
+// this.shooter = shooter;
+// this.magazine = magazine;
+// this.intake = intake;
+// addRequirements(shooter, magazine, intake, turret);
 
-        PathPlannerTrajectory trajectory = PathPlanner.loadPath("P_2B", 1, 1);
-        PPSwerveControllerCommand autoDrive = baseSwerveCommand(trajectory);
+// PathPlannerTrajectory trajectory = PathPlanner.loadPath("P_2B", 1, 1);
+// PPSwerveControllerCommand autoDrive = baseSwerveCommand(trajectory);
 
-        // ShooterRPM shooterCommand = new ShooterRPM(shooter, vision);
-        addCommands(new InstantCommand(() -> swerve.resetOdometry(trajectory.getInitialPose())),
-            new InstantCommand(() -> turret.alignEnabled = true),
-            new InstantCommand(() -> this.shooter.setSetpoint(4500 / 60)),
-            new InstantCommand(() -> this.shooter.enable()),
-            new ParallelDeadlineGroup(
-                new SequentialCommandGroup(
-                    new ParallelCommandGroup(new InstantCommand(() -> intake.intakeDeploy()),
-                        autoDrive),
-                    new ZeroMotorsWaitCommand(swerve, 1),
-                    new InstantCommand(() -> intake.intakeRetract()),
-                    new ZeroMotorsWaitCommand(swerve, 10)),
-                new SequentialCommandGroup(new WaitCommand(2),
-                    new WaitUntilCommand(() -> shooter.getSetpoint() > 0 && shooter.atSetpoint()),
-                    new InstantCommand(() -> magazine.enable())),
-                new SequentialCommandGroup(
-                    new ParallelDeadlineGroup(new WaitCommand(.6),
-                        new InstantCommand(() -> turret.turretLeft())),
-                    new AlignTurret(turret, vision))));
-    }
+// // ShooterRPM shooterCommand = new ShooterRPM(shooter, vision);
+// addCommands(new InstantCommand(() -> swerve.resetOdometry(trajectory.getInitialPose())),
+// new InstantCommand(() -> turret.alignEnabled = true),
+// new InstantCommand(() -> this.shooter.setSetpoint(4500 / 60)),
+// new InstantCommand(() -> this.shooter.enable()),
+// new ParallelDeadlineGroup(
+// new SequentialCommandGroup(
+// new ParallelCommandGroup(new InstantCommand(() -> intake.intakeDeploy()),
+// autoDrive),
+// new ZeroMotorsWaitCommand(swerve, 1),
+// new InstantCommand(() -> intake.intakeRetract()),
+// new ZeroMotorsWaitCommand(swerve, 10)),
+// new SequentialCommandGroup(new WaitCommand(2),
+// new WaitUntilCommand(() -> shooter.getSetpoint() > 0 && shooter.atSetpoint()),
+// new InstantCommand(() -> magazine.enable())),
+// new SequentialCommandGroup(
+// new ParallelDeadlineGroup(new WaitCommand(.6),
+// new InstantCommand(() -> turret.turretLeft())),
+// new AlignTurret(turret, vision))));
+// }
 
-    @Override
-    public void end(boolean interrupted) {
-        magazine.disable();
-        shooter.disable();
-        intake.intakeRetract();
-    }
-}
+// @Override
+// public void end(boolean interrupted) {
+// magazine.disable();
+// shooter.disable();
+// intake.intakeRetract();
+// }
+// }
--- src/main/java/frc/robot/autos/TestNoZeroGyroAuto.java
+++ src/main/java/frc/robot/autos/TestNoZeroGyroAuto.java
@@ -1,46 +1,46 @@
-package frc.robot.autos;
+// package frc.robot.autos;
 
-import java.util.List;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import frc.robot.Constants;
-import frc.robot.commands.ZeroMotorsWaitCommand;
-import frc.robot.modules.AutoBase;
-import frc.robot.modules.Vision;
-import frc.robot.subsystems.Swerve;
+// import java.util.List;
+// import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.geometry.Rotation2d;
+// import edu.wpi.first.math.trajectory.Trajectory;
+// import edu.wpi.first.math.trajectory.TrajectoryConfig;
+// import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+// import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+// import frc.robot.Constants;
+// import frc.robot.commands.ZeroMotorsWaitCommand;
+// import frc.robot.modules.AutoBase;
+// import frc.robot.modules.Vision;
+// import frc.robot.subsystems.Swerve;
 
-/**
- * Autonomous that aligns limelight then excecutes a trajectory.
- */
-public class TestNoZeroGyroAuto extends AutoBase {
-    Vision vision;
+// /**
+// * Autonomous that aligns limelight then excecutes a trajectory.
+// */
+// public class TestNoZeroGyroAuto extends AutoBase {
+// Vision vision;
 
-    /**
-     * Autonomous that aligns limelight then excecutes a trajectory.
-     *
-     * @param swerve swerve subsystem
-     * @param vision vision subsystem
-     */
-    public TestNoZeroGyroAuto(Swerve swerve, Vision vision) {
-        super(swerve);
-        System.out.println("Limelight Auto !!");
-        TrajectoryConfig config =
-            new TrajectoryConfig(Constants.AutoConstants.kMaxSpeedMetersPerSecond,
-                Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                    .setKinematics(Constants.Swerve.swerveKinematics);
+// /**
+// * Autonomous that aligns limelight then excecutes a trajectory.
+// *
+// * @param swerve swerve subsystem
+// * @param vision vision subsystem
+// */
+// public TestNoZeroGyroAuto(Swerve swerve, Vision vision) {
+// super(swerve);
+// System.out.println("Limelight Auto !!");
+// TrajectoryConfig config =
+// new TrajectoryConfig(Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+// Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+// .setKinematics(Constants.Swerve.swerveKinematics);
 
-        Trajectory firstHalfTrajectory = TrajectoryGenerator.generateTrajectory(
-            List.of(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(0.1, 0, new Rotation2d(0)),
-                new Pose2d(0.1, 0.1, new Rotation2d(0))),
-            config);
+// Trajectory firstHalfTrajectory = TrajectoryGenerator.generateTrajectory(
+// List.of(new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(0.1, 0, new Rotation2d(0)),
+// new Pose2d(0.1, 0.1, new Rotation2d(0))),
+// config);
 
-        SwerveControllerCommand firstHalfTraject = baseSwerveCommand(firstHalfTrajectory);
-        ZeroMotorsWaitCommand firstWait = new ZeroMotorsWaitCommand(swerve, 3);
+// SwerveControllerCommand firstHalfTraject = baseSwerveCommand(firstHalfTrajectory);
+// ZeroMotorsWaitCommand firstWait = new ZeroMotorsWaitCommand(swerve, 3);
 
-        addCommands(firstHalfTraject, firstWait);
-    }
-}
+// addCommands(firstHalfTraject, firstWait);
+// }
+// }
