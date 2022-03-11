7e5a57e1ad31385229419d6c810bb9a7bde3b0a7
agrinmanriv0537 <agrinmanriv@gmail.com>
2022-03-10 22:41:51 -0600

    need to test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,7 +27,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
         ctreConfigs = new CTREConfigs();
-        CameraServer.getInstance().startAutomaticCapture();
+        CameraServer.startAutomaticCapture();
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our
         // autonomous chooser on the dashboard.
