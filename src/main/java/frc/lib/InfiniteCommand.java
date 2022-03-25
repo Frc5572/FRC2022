83ac39f6adc0dc1175404b3981b22ef906bf468a
Owen <owenjflatman@gmail.com>
2022-03-24 19:23:39 -0500

    Linting

--- src/main/java/frc/lib/InfiniteCommand.java
+++ src/main/java/frc/lib/InfiniteCommand.java
@@ -4,10 +4,19 @@ import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+/**
+ * New command that only runs execute.
+ */
 public class InfiniteCommand extends CommandBase {
 
     protected final Runnable m_onExecute;
 
+    /**
+     * Adds execute requirements.
+     *
+     * @param onExecute
+     * @param requirements
+     */
     public InfiniteCommand(Runnable onExecute, Subsystem... requirements) {
         m_onExecute = requireNonNullParam(onExecute, "onExecute", "InfiniteCommand");
 
