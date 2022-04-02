7ed519fbe45fe4a337a4737b19f01f5adfba5cbe
Owen <owenjflatman@gmail.com>
2022-04-01 19:29:03 -0500

    made trigger for magnet sensor

--- src/main/java/frc/robot/subsystems/InsideClimber.java
+++ src/main/java/frc/robot/subsystems/InsideClimber.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticHub;
@@ -15,6 +16,7 @@ import frc.robot.Constants;
 public class InsideClimber extends SubsystemBase {
 
     private final DoubleSolenoid insideClimberSolenoid;
+    public DigitalInput climbeSense = new DigitalInput(0);
 
     private final WPI_TalonFX insideClimberMotor1 =
         new WPI_TalonFX(Constants.Motors.insideClimberMotorRightId, "canivore");
