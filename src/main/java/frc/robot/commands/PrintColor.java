d06495bd11896596a767959ecda9d5da3fe0c2e8
agrinmanriv0537 <agrinmanriv@gmail.com>
2022-09-02 16:14:53 -0500

    raw rgb value data in comment in code

--- src/main/java/frc/robot/commands/PrintColor.java
+++ src/main/java/frc/robot/commands/PrintColor.java
@@ -17,9 +17,39 @@ public class PrintColor extends CommandBase {
     public void execute() {
         ColorSensorV3.RawColor color = colorSensor.getRawColor();
         SmartDashboard.putNumber("Red", color.red);
-        SmartDashboard.putNumber("Blue", color.blue);
         SmartDashboard.putNumber("Green", color.green);
+        SmartDashboard.putNumber("Blue", color.blue);
 
+        /*
+         * Blue(rapid react ball):
+         * 
+         * 
+         * 1in: R:670 G:1817 B:2193
+         * 
+         * .5in: R:970 G:2677 B:3355
+         * 
+         * touching: R:1572 G:4487 B:5615
+         * 
+         * 
+         * Red(rapid react ball):
+         * 
+         * 
+         * 1in: R:878 G:604 B:245
+         * 
+         * .5in: R:2229 G:508 B:1360
+         * 
+         * touching: R:8236 G:4746 B:1636
+         * 
+         * 
+         * Yellow(shiny infinite recharge ball):
+         * 
+         * 
+         * 1in: R:4558 G:7737 B:1231
+         * 
+         * .5in: R:13216 G:22408 B:3425
+         * 
+         * touching: R:18711 G:31612 B:4468
+         */
 
         System.out.println(colorSensor.getColorString());
     }
